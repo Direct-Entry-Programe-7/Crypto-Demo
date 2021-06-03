5f9a3a04e779d23d975dca786c3558005d062ce2
Nethmina <msenarathne96@gmail.com>
2021-06-03 21:13:47 +0530

    UI Design

--- src/controller/DecryptionController.java
+++ src/controller/DecryptionController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class DecryptionController {
+    public TextField txtCipherText;
+    public TextField txtKey;
+    public TextField txtOriginalText;
+
+    public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/EncryptionController.java
+++ src/controller/EncryptionController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class EncryptionController {
+    public TextField txtCipher;
+    public TextField txtKey;
+    public TextField txtText;
+
+    public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+    }
 }
